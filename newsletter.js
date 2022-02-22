440718d76251ce0627437c5057404cf35236686f
Jim <chengxuyuan9527@gmail.com>
2022-02-23 00:14:40 +1100

    First Commit

---
+++ newsletter.js
@@ -0,0 +1,83 @@
+import path from "path";
+import {
+  fileURLToPath
+} from "url";
+const __filename = fileURLToPath(
+  import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import https from "https";
+
+import express from "express";
+const app = express()
+app.use(express.static("public")); //css and image are local make them public. realtive to public
+
+import bodyParser from "body-Parser";
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res) {
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+  var data = {
+    email_address: email,
+    status: "subscribed",
+    merge_fields: {
+      FNAME: firstName,
+      LNAME: lastName,
+    }
+  }
+
+
+  var jsonData = JSON.stringify(data);
+
+  const url = "https://us14.api.mailchimp.com/3.0/lists/cd57a714fe"
+  const options = {
+    method: "POST",
+    auth: "apikey:468bb192b5db951f26ae6363c01e61e4-us14"
+  }
+
+  // node.js standard request module. send post request to mailchimp
+  const request = https.request(url, options, function(response) {
+
+    if (request.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html")
+    } else {
+      res.sendFile(__dirname + "/failure.html")
+    }
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+});
+
+//redirect to home route
+app.post("/failure", function(req, res) {
+
+  res.redirect("/");
+
+})
+
+
+
+
+
+app.listen(process.env.PORT || 3000 , function() {
+  console.log("Server is currently running on port 3000")
+})
+
+
+// const maichimpAPI = "468bb192b5db951f26ae6363c01e61e4-us14"
+
+//cd57a714fe
